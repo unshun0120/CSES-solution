65155287bd77f47dbda2955f75485a769ccfad18
unshun0120 <79517348+unshun0120@users.noreply.github.com>
2024-10-08 09:11:20 +0800

    Add files via upload

---
+++ Introductory Problems/Increasing Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	long long i, n, move=0;
+	
+	cin >> n;
+	long long arr[n];
+	for(i = 0; i < n; i++)
+		cin >> arr[i];
+	
+	for(i = 0; i < n-1; i++){
+		if(arr[i] > arr[i+1]){
+			move += arr[i]-arr[i+1];
+			arr[i+1] = arr[i];
+		}
+	}
+	cout << move;
+	
+	return 0;	
+}
